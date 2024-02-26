59a570a13c96ed52984b0a76af49d42071d539d8
mishi <mishi.beraia@gmail.com>
2024-02-26 23:39:29 +0400

    Add links, specify language, limit number of news, un-hardcode the topic.

--- main.py
+++ main.py
@@ -1,20 +1,21 @@
 import requests
 from functions import send_email
 
+topic = "apple"
 api_key = "60b6d83018ef4f4a912afb95313925ae"
-url = ("https://newsapi.org/v2/everything?q=apple&from=2024-02-"
+url = (f"https://newsapi.org/v2/everything?q={topic}&from=2024-02-"
        "25&to=2024-02-25&sortBy=popularity&apiKey=60b6d83018ef4f"
-       "4a912afb95313925ae")
+       "4a912afb95313925ae&language=en")
 # Make request
 request = requests.get(url)
 
 # Get a dictionary with data
 content = request.json()
-message = ""
-for article in content["articles"]:
+message = "Subject: Today's News" + "\n"
+for article in content["articles"][0:20]:
     title = (article["title"]).strip()
     desc = (article["description"]).strip()
-    message = message + "title: " + title + "\n" + "desc: " + desc + 2*"\n"
+    message = message + "title: " + title + "\n" + "desc: " + desc + f"URL:{article['url']}" + 2*"\n"
 
 message = message.encode('utf-8')
 send_email(message)
